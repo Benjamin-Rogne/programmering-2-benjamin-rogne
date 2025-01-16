2cd2ec3c1ae84137326e8da0498ce503b3467bd7
Benjamin-Rogne <benrog27@gmail.com>
2025-01-16 17:11:59 +0100

    test

---
+++ Test/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Velkommen til programmet!");
+
+        // Kall metoden for å beregne vekt på månen
+        beregnVektPaaMaanen();
+    }
+
+        // Skriv ut resultatet
+        System.out.printf("Med en vekt på jorden på %.2f kilo, vil din vekt på månen være ca. %.2f kilo.%n", jordVekt, maanVekt);
+    }
+}
