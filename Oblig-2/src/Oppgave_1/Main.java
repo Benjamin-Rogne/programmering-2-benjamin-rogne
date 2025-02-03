d810a32def52bf31098e8067c89ff1eb28b37ff7
Benjamin-Rogne <benrog27@gmail.com>
2025-02-04 00:13:26 +0100

    ok

--- Oblig-2/src/Oppgave_1/Main.java
+++ Oblig-2/src/Oppgave_1/Main.java
@@ -1,7 +1,31 @@
 package Oppgave_1;
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // Oppretter en TV-serie med LocalDate
+        TVSeries series = new TVSeries("Game of Code", "A show about programming warriors.", LocalDate.of(2025, 3, 15));
+
+        // Oppretter episoder
+        Episode ep1 = new Episode("Pilot", 1, 1, 42);
+        Episode ep2 = new Episode("The Bug Awakens", 2, 1); // Runtime blir 0 (ukjent)
+
+        // Legger til episoder i TV-serien
+        series.addEpisode(ep1);
+        series.addEpisode(ep2);
+
+        // `toString()` is automatically called here:
+        System.out.println(series);
+
+        System.out.println("\nEpisodes:");
+        for (Episode episode : series.getEpisodes()) {
+            System.out.println(episode + "\n"); // `toString()` is automatically called
+        }
+
+        // Viser informasjon om TV-serien
+        //series.displaySeriesInfo();
+
+        // Viser alle episoder
+        //series.displayAllEpisodes();
     }
 }
--- Oblig-2/src/Oppgave_1/TVSeries.java
+++ Oblig-2/src/Oppgave_1/TVSeries.java
@@ -8,4 +8,61 @@ public class TVSeries {
     private String description;
     private LocalDate releaseDate;
     private ArrayList<Episode> episodes;
+
+    // konstruktør
+    public TVSeries(String title, String description, LocalDate releaseDate) {
+        this.title = title;
+        this.description = description;
+        this.releaseDate = releaseDate;
+        this.episodes = new ArrayList<>();
+    }
+
+    // legg til episode
+    public void addEpisode(Episode episode) {
+        episodes.add(episode);
+    }
+
+    // Getter-metoder
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public ArrayList<Episode> getEpisodes() {
+        return new ArrayList<>(episodes); // Returnerer en kopi for sikkerhet
+    }
+
+    // Setter-metoder
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+    // Vise informasjon om TV-serien
+    public void displaySeriesInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Description: " + description);
+        System.out.println("Release Date: " + releaseDate);
+        System.out.println("Number of Episodes: " + episodes.size());
+    }
+    @Override
+    public String toString() {
+        return "TV series Title: " + title + "\n" +
+                "Description: " + description + "\n" +
+                "Release date: " + releaseDate + "\n" +
+                "Number of episodes: " + episodes.size();
+    }
 }
